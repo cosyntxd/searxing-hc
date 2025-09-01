5f5fd21e9011d68f0c0db462ba046b5aa63e10a9
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2025-09-01 14:19:24 -0700

    script for word list

--- scripts/popular_word.py
+++ scripts/popular_word.py
@@ -1,5 +1,7 @@
+import re
 import json
 from collections import Counter
+from pathlib import Path
 
 # todo: relative
 with open("/Users/ryan/Github/Hackclub-projects/complete_database.json") as f:
@@ -10,7 +12,7 @@ counter = Counter()
 def collect_values(obj, parent_key=None):
     if isinstance(obj, dict):
         for k, v in obj.items():
-            if k in ["extra", "updates", "description"]:
+            if k in ["extra", "author", "main_image"]:
                 continue
             collect_values(v, k)
     elif isinstance(obj, list):
@@ -19,10 +21,16 @@ def collect_values(obj, parent_key=None):
     else:
         if not isinstance(obj, int):
             if obj:
-                for word in obj.split(" "):
-                    counter[word.lower()] += 1
+                if not "//" in obj:
+                    cleaned = re.sub(r"[^A-Za-z0-9\s]", "", obj)
+                    for word in cleaned.split(" "):
+                        if not any(c.isdigit() for c in word):
+                            if word:
+                                counter[word.lower()] += 1
 
 collect_values(data)
 
-for value, count in counter.most_common(33):
-    print(f"{value}:{count}")
+out_file = Path(__file__).parent.parent / "data" / "word_list.txt"
+with open(out_file, "w") as f:
+    for value, count in counter.most_common(20_000):
+        f.write(f"{value}\n")
